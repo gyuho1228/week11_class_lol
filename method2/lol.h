0d70f51e5fb9e3149e8d21af31643cc2cd900b88
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2023-11-15 15:32:42 +0900

    Create lol.h

---
+++ method2/lol.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <fstream>
+#include<iostream>
+using namespace std;
+class LOL {
+public:
+	LOL() {};
+	LOL(int n1, int n2, float aa, float bb, float xx0, float ddx, int mm) {
+		N1 = n1;
+		N2 = n2;
+		a = aa;
+		b = bb;
+		x0 = xx0;
+		dx = ddx;
+		m = mm;
+		f1d = new int[N2 - N1 + 1];
+		f2d = new float[m];
+	}
+	~LOL() {};  //이거만 지우면 오류 x, destructor
+
+	int N1, N2;
+	float a, b, x0, dx;
+	int m;
+	int* f1d;
+	float* f2d;
+
+	ofstream xx;
+	ifstream aa;
+
+	void f0(string);
+	void f1();
+	void f2();
+	void f3(string);
+	void ff(string);
+};
