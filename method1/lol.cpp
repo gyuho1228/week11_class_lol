7cfebe887c29f170af7589e7800809730e4dce19
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2023-11-15 13:53:04 +0900

    Update lol.cpp

--- method1/lol.cpp
+++ method1/lol.cpp
@@ -3,21 +3,17 @@
 #include "lol.h"
 void LOL::f0(string ss) {
 	xx.open(ss, ios::binary | ios::out);
-
 }
 
 void LOL::f1() {
 	int* f1d;
 	f1d = new int[N2 - N1 + 1];
-	
 	for (int i = N1; i < N2 + 1; i++) {
 		f1d[i-N1] = (i * (i + 1))/ 2;
 	}
-	cout << endl;
-	xx.write((char*)f1d	, sizeof(int) * (N2 - N1 + 1));
+	xx.write((char*)f1d, sizeof(int) * (N2 - N1 + 1));
 	delete[] f1d;
 }
-
 	
 void LOL::f2() {
 	float* f2d, x;
@@ -30,6 +26,7 @@ void LOL::f2() {
 	delete[] f2d;
 	xx.close();
 }
+
 void LOL::f3(string ss) {	//binary파일을 읽어서 cout 하기
 
 	int* a;
