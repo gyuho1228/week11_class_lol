10ec3032827905f76a90dc8272618293214b2ede
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2023-11-15 14:01:46 +0900

    Update lol.cpp

--- method1/lol.cpp
+++ method1/lol.cpp
@@ -3,17 +3,29 @@
 #include "lol.h"
 void LOL::f0(string ss) {
 	xx.open(ss, ios::binary | ios::out);
+	xx.write((char*)&N1, sizeof(int));
+	xx.write((char*)&N2, sizeof(int));
+	xx.write((char*)&a, sizeof(float));
+	xx.write((char*)&b, sizeof(float));
+	xx.write((char*)&x0, sizeof(float));
+	xx.write((char*)&dx, sizeof(float));
+	xx.write((char*)&m, sizeof(int));
+
+
 }
 
 void LOL::f1() {
 	int* f1d;
 	f1d = new int[N2 - N1 + 1];
+	
 	for (int i = N1; i < N2 + 1; i++) {
 		f1d[i-N1] = (i * (i + 1))/ 2;
 	}
-	xx.write((char*)f1d, sizeof(int) * (N2 - N1 + 1));
+	cout << endl;
+	xx.write((char*)f1d	, sizeof(int) * (N2 - N1 + 1));
 	delete[] f1d;
 }
+
 	
 void LOL::f2() {
 	float* f2d, x;
@@ -26,25 +38,39 @@ void LOL::f2() {
 	delete[] f2d;
 	xx.close();
 }
-
-void LOL::f3(string ss) {	//binary파일을 읽어서 cout 하기
+void LOL::f3(string ss) {
 
 	int* a;
 	float* b;
 	a = new int[N2 - N1 + 1];
 	b = new float[m];
-
+	
+	int a1[2], a3;
+	float a2[4];
 
 	aa.open(ss, ios::binary | ios::in);
-
+	aa.read((char*)a1, sizeof(a1));
+	aa.read((char*)a2, sizeof(a2));
+	aa.read((char*)&a3, sizeof(a3));
 	aa.read((char*)a, sizeof(int) * (N2 - N1 + 1));
 	aa.read((char*)b, sizeof(float) * m);
 	
+	cout << "a b N1 N2 x0 dx m" << endl;
+	for (auto k : a1) {
+		cout << k << " ";
+	}
+	for (auto k : a2) {
+		cout << k << " ";
+	}
+	cout << a3 << endl;
+
+	cout << "f1()" << endl;
+
 	for (int i = 0; i < (N2 - N1 + 1); i++) {
 		cout << a[i] << " ";
 	}
 
-	cout << endl;
+	cout << endl<< "f2()" << endl;;
 
 	for (int i = 0; i < m; i++) {
 		cout << b[i] << " ";
