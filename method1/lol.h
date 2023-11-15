f684671f8b460cada4cc1e4a0311102357e57acc
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2023-11-15 13:43:54 +0900

    Create lol.h

---
+++ method1/lol.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <fstream>
+#include<iostream>
+using namespace std;
+class LOL {
+public:
+	LOL() {};
+
+	int N1, N2;
+	float a, b, x0, dx;
+	int m;
+
+	ofstream xx;
+	ifstream aa;
+
+	void f0(string);
+	void f1();
+	void f2();
+	void f3(string);
+};
