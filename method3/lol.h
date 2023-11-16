5d061d648162a9635328ef743c6d018f9b5f05b0
gyuho1228 <144807315+gyuho1228@users.noreply.github.com>
2023-11-16 16:04:37 +0900

    Create lol.h

---
+++ method3/lol.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <fstream>
+#include<iostream>
+using namespace std;
+class LOL {
+public:
+	LOL() { f1d = nullptr; f2d = nullptr; }
+	LOL(int n1, int n2, float aa, float bb, float xx0, float ddx, int mm, string s) {
+		N1 = n1;
+		N2 = n2;
+		a = aa;
+		b = bb;
+		x0 = xx0;
+		dx = ddx;
+		m = mm;
+		ss = s;
+		f1d = new int[N2 - N1 + 1];
+		f2d = new float[m];
+	}
+	~LOL() {
+		delete[] f1d;
+		delete[] f2d;
+	}
+
+
+
+	int N1, N2;
+	float a, b, x0, dx;
+	int m;
+	int* f1d;
+	float* f2d;
+	string ss;
+	ofstream xx;
+	ifstream aa;
+
+	void f0(string);
+	void f1();
+	void f2();
+	void f3();
+	void ff(string);
+};
